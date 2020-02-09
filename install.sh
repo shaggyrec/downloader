3740f9aa912c173c82f04f71c7fec2d405a7ff2b
Alexander Shogenov <i@shagg.ru>
2020-02-09 18:46:08 +0300

    Add travis

--- install.sh
+++ install.sh
@@ -1,9 +1,6 @@
 #!/usr/bin/env bash
 
-DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
-
 composer install
 npm run production
-touch ${DIR}/database/db.sqlite
-echo "DB_DATABASE=${DIR}/database/db.sqlite" >> .env
-php artisan migrate
+./createdb.sh
+
